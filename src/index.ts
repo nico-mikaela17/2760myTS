0ac65a3f0869f47a9dabb4f90c98962b1df53478
Nicole Linnarz <mikaelafs17@gmail.com>
2023-02-07 22:35:36 -0700

    will it work?

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 
-document.querySelector('#counterDiv')!.innerHTML = `
+/*document.querySelector('#counterDiv')!.innerHTML = `
 <a class="text-3xl" href="https://www.uvu.edu" target="_blank">Go to UVU!</a>
-` 
+` */
 
 export function logSomething() {
   console.log('Main index.ts file reporting for duty!')
